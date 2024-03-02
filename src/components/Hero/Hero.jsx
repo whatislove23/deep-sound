46542fdd1d5b50f34a35120ab3e9083241d7698f
Vladyslav Tymchenko <v.tymchenko.23@gmail.com>
2024-03-02 22:28:19 +0200

    vercel test

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
       <Container>
         <h1 style={{ marginTop: "100px" }}>HERO SECTION</h1>
         <HashLink smooth to="/#footer" style={{ fontSize: "28px" }}>
-          to Footer...1234 vlad 122333
+          to Footer...1234 vlad 122333 hello vercel
         </HashLink>
         <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus,
