fe7b38256d669cca2a12031c8f987fffce904535
SerhiiMonzhelesov <monzhelesov@gmail.com>
2024-03-02 22:15:15 +0200

    com

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
       <Container>
         <h1 style={{ marginTop: "100px" }}>HERO SECTION</h1>
         <HashLink smooth to="/#footer" style={{ fontSize: "28px" }}>
-          to Footer...1234 vlad
+          to Footer
         </HashLink>
         <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus,
