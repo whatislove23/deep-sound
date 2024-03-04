e9373c5b80a3c30ec83beb2a84ec8165ee38e6a9
mifodiy911 <mifodiy911@gmail.com>
2024-03-04 17:56:28 +0200

    complete Blog component

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,13 +1,44 @@
+import { useState } from 'react';
 import Container from "../Container/Container";
 import { StyledBlog } from "./StyledBlog.styled";
+import BlogItem from '../BlogItem/BlogItem';
 
 function Blog() {
+	const [blogs, setBlogs] = useState([{
+		id: 1,
+		link: '/blog/1',
+		title: 'Beats Pro 2 - Full Review',
+		imgUrl: 'src/assets/blog/blog-one.jpg'
+	},
+	{
+		id: 2,
+		link: '/blog/2',
+		title: 'Stephen Kulik - What a good sound should be?',
+		imgUrl: 'src/assets/blog/blog-two.jpg'
+	},
+	{
+		id: 3,
+		link: '/blog/3',
+		title: 'Your home studio - what is the minimum price?',
+		imgUrl: 'src/assets/blog/blog-three.jpg'
+	},
+	{
+		id: 4,
+		link: '/blog/4',
+		title: 'Understanding Compression A Full User Guide',
+		imgUrl: 'src/assets/blog/blog-four.jpg'
+	}])
 	return (
-		<Container>
-			<StyledBlog>
-
-			</StyledBlog>
-		</Container>
+		<StyledBlog>
+			<Container>
+				<div className="blog_inner">
+					{blogs.map(blog => (
+						<BlogItem key={blog.id} {...blog}/>
+					))}
+				</div>	
+			</Container>
+		</StyledBlog>
+	
 	)
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import Hero from "../components/Hero/Hero";
 import NewArrives from "../components/NewArrives/NewArrives";
-import NoName from "../components/NoName/NoName";
 import OnSale from "../components/OnSale/OnSale";
 import SubscribeForm from "../components/SubscribeForm/SubscribeForm";
+import Blog from "../components/Blog/Blog";
 
 function Home() {
   return (
@@ -10,7 +10,7 @@ function Home() {
       <Hero />
       <NewArrives />
       <OnSale />
-      <NoName />
+      <Blog/>
       <SubscribeForm />
     </main>
   );
