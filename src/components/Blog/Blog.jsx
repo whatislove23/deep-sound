7b7a9e8c7732ce8d8cb4471b2aeffd46220d04d7
mifodiy911 <mifodiy911@gmail.com>
2024-03-04 13:18:57 +0200

    Add Blog component

---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,14 @@
+import Container from "../Container/Container";
+import { StyledBlog } from "./StyledBlog.styled";
+
+function Blog() {
+	return (
+		<Container>
+			<StyledBlog>
+
+			</StyledBlog>
+		</Container>
+	)
+}
+
+export default Blog
