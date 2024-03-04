d4ac82207aa6df2796c8932fc29305dbe51f5a47
mifodiy911 <mifodiy911@gmail.com>
2024-03-04 17:53:57 +0200

    complete BlogItem component

--- src/components/BlogItem/BlogItem.jsx
+++ src/components/BlogItem/BlogItem.jsx
@@ -1,11 +1,28 @@
-import { StyledBlogItem } from './StyledBlogItem.styled'
+import { NavLink } from 'react-router-dom';
+import { StyledBlogButton, StyledBlogItem } from './StyledBlogItem.styled'
+import PropTypes from "prop-types";
 
-function BlogItem() {
+function BlogItem({link, title, imgUrl}) {
 	return (
 		<StyledBlogItem>
-
+			<img className='blog_img' src={imgUrl} alt={title} />
+			<div className="blog_wrapper">
+				<h2 className="blog_title">{title}</h2>
+				<NavLink to={link}>
+					<StyledBlogButton>
+						Explore
+					</StyledBlogButton>
+				</NavLink>
+			</div>
 		</StyledBlogItem>
 	)
 }
+
+BlogItem.propTypes = {
+  id: PropTypes.number,
+	link: PropTypes.string,
+	title: PropTypes.string,
+	imgUrl: PropTypes.string
+};
 
 export default BlogItem
--- src/components/BlogItem/StyledBlogItem.styled.js
+++ src/components/BlogItem/StyledBlogItem.styled.js
@@ -1,5 +1,48 @@
 import styled from "styled-components";
+import Button from '../Button/Button';
 
 export const StyledBlogItem = styled.div`
+	width: 100%;
+	position: relative;
+	overflow: hidden;
 
+	.blog_img {
+		width: 100%;
+		transition: transform ease-in-out .5s;
+	}
+
+	.blog_wrapper {
+		position: absolute;
+		left: 0;
+		bottom: 0;
+		width: 100%;
+		height: 192px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: space-between;
+		gap: 10px;
+		background: rgba(12, 11, 11, .7);
+		padding: 40px 40px 24px;
+		color: var(--secondary-text);
+	}
+
+	.blog_title {
+		text-align: center;
+		height: 67px;
+		display: flex;
+		align-items: center;
+		font-size: 28px;
+		line-height: 1.2;
+		font-weight: 400;
+	}
+
+	&:hover .blog_img {
+		transform: scale(1.5);
+	}
+`
+
+export const StyledBlogButton = styled(Button)`
+	width: 273px;
+	height: 32px;
 `
