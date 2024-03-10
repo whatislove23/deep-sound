696e22362b14a3a3215217ad0920d2b9866ce01e
mifodiy911 <mifodiy911@gmail.com>
2024-03-10 19:44:24 +0200

    fix StyledBlogButton and StyledLinkShowMore

--- src/components/BlogItem/BlogItem.jsx
+++ src/components/BlogItem/BlogItem.jsx
@@ -1,6 +1,6 @@
-import { NavLink } from 'react-router-dom';
 import { StyledBlogButton, StyledBlogItem } from './StyledBlogItem.styled'
 import PropTypes from "prop-types";
+
 
 function BlogItem({link, title, imgUrl}) {
 	return (
@@ -8,11 +8,9 @@ function BlogItem({link, title, imgUrl}) {
 			<img className='blog_img' src={imgUrl} alt={title} />
 			<div className="blog_wrapper">
 				<h2 className="blog_title">{title}</h2>
-				<NavLink to={link}>
-					<StyledBlogButton>
-						Explore
-					</StyledBlogButton>
-				</NavLink>
+				<StyledBlogButton to={link}>
+					Explore
+				</StyledBlogButton>
 			</div>
 		</StyledBlogItem>
 	)
--- src/components/NewArrives/StyledNewArrives.styled.js
+++ src/components/NewArrives/StyledNewArrives.styled.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
-import { NavLink } from "react-router-dom";
-import { StandartBtnDark } from "../Button/StyledButton.styled";
+import { StyledNavLinkButton } from "../Button/StyledButton.styled";
 export const StyledNewArrives = styled.section`
   h2 {
     font-weight: 400;
@@ -15,19 +14,11 @@ export const StyledNewArrives = styled.section`
     row-gap: 40px;
   }
 `;
-export const StyledLinkShowMore = styled(NavLink)`
-  ${StandartBtnDark}
+export const StyledLinkShowMore = styled(StyledNavLinkButton)`
   width: 414px;
   height: 48px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 0 auto;
-  background-color: var(--secondary-bgcolor);
-  color: var(--secondary-text);
   font-size: 20px;
   /* line-height: 24;ß */
   font-weight: 500;
   border-radius: 30px;
-  text-decoration: none;
 `;
