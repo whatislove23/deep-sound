37b03cae9b1f7bd3d72bbc10eec04704610a8f7e
mifodiy911 <mifodiy911@gmail.com>
2024-03-20 20:51:57 +0200

    fix Stars component:  change condition

--- src/components/Stars/Stars.jsx
+++ src/components/Stars/Stars.jsx
@@ -7,9 +7,10 @@ export default function Stars({ rating, width = 12, gap = 2, ...props }) {
     res.push(<StyledStar $width={width} $gap={gap} key={i} className="fullStar" />);
   }
   let rest = Number("0." + (rating.toString().split(".")[1] || 0));
-  if (rest <= 0.5)
-    res.push(<StyledStarHalf $width={width} $gap={gap} key={res.length + 1} />);
-  if (rest >= 0.7) res.push(<StyledStar $width={width} $gap={gap} key={res.length + 1} />);
+  if (rest <= 0.5 && rest !== 0)
+  res.push(<StyledStarHalf $width={width} $gap={gap} key={res.length + 1} />);
+  if (rest > 0.5) 
+  res.push(<StyledStar $width={width} $gap={gap} key={res.length + 1} />);
   return <div {...props}>{res}</div>;
 }
 
