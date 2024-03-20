17e67778bdf703cebc750ad9e82fbc508a70e2e4
mifodiy911 <mifodiy911@gmail.com>
2024-03-20 20:19:32 +0200

    fix Stars component:  add width and gap props

--- src/components/Stars/StyledStars.styled.js
+++ src/components/Stars/StyledStars.styled.js
@@ -3,17 +3,17 @@ import { IoStarSharp, IoStarHalfSharp } from "react-icons/io5";
 import Container from "../Container/Container";
 
 export const StyledStarHalf = styled(IoStarHalfSharp)`
-  width: 12px;
-  height: 12px;
+  width: ${({$width}) => ($width)}px;
+  height: ${({$width}) => ($width)}px;
   color: var(--accent-yellow);
 `;
 export const StyledStar = styled(IoStarSharp)`
-  width: 12px;
-  height: 12px;
+  width: ${({$width}) => ($width)}px;
+  height: ${({$width}) => ($width)}px;
   color: var(--accent-yellow);
 `;
 export const StyledStarContainer = styled(Container)`
   display: flex;
   width: 68px;
-  gap: 2px;
+  gap: ${({$gap}) => ($gap)}px;
 `;
